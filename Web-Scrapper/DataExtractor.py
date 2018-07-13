c115cb184d9633d8933b617b72404f80409736db
DV <me@iamdv.io>
2018-07-13 03:09:41 +0100

    Created Jupyter Notebook for Exploratory Data Analysis Playing with the web-scrapper file

--- Web-Scrapper/DataExtractor.py
+++ Web-Scrapper/DataExtractor.py
@@ -1,1 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import html5lib
 
+source = requests.get('https://www.coursera.org/learn/python').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+print(soup.prettify())
