5a6364835b3371160dd4aa9551d5d3f02707eb9a
DV <me@iamdv.io>
2018-07-22 17:20:52 +0100

    Added logic to extract the data from DIV class

--- Web-Scrapper/DataExtractor.py
+++ Web-Scrapper/DataExtractor.py
@@ -3,8 +3,17 @@ import requests
 import lxml
 import html5lib
 
-source = requests.get('https://www.coursera.org/learn/python-data').text
+# source = requests.get('https://www.coursera.org/learn/python-data').text
+source = requests.get('https://www.coursera.org/learn/machine-learning').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-print(soup.prettify().encode('utf-8'))
+# print(soup.prettify().encode('utf-8'))
+
+body = soup.find('body')
+summary = body.find('div', class_ = 'bt3-col-sm-9')
+print(summary.prettify().encode('utf-8'))
+
+
+#bt3-col-sm-9
+# #266
