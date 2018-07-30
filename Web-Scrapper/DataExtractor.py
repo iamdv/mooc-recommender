a47789aaea31d3b50804788d25d25d247115f6da
Deepak Vadithala <dv@deepaks-imac.home>
2018-07-31 00:22:45 +0100

    Added logic to extract breadcrumb for one course

--- Web-Scrapper/DataExtractor.py
+++ Web-Scrapper/DataExtractor.py
@@ -1,5 +1,5 @@
 #!/Users/dv/anaconda/bin/python3
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
 import requests
@@ -8,34 +8,63 @@ import html5lib
 import json
 import csv
 
+
+#############################################################################
 ''' 
 getCourseListing function extracts all the courses listed
 and we save the courses in the course_listing.csv file 
 in the data folder.
 '''
 
+
 def getCourseListing():
-    with open("./Data/course_listing.csv", 'w', newline= '') as myCSVFile:
+    with open("./Data/course_listing.csv", 'w', newline='') as myCSVFile:
         fieldnames = ['ID', 'FullURI', 'ProductBadge', 'CourseDescription']
-        thewriter = csv.DictWriter(myCSVFile, fieldnames= fieldnames)
+        thewriter = csv.DictWriter(myCSVFile, fieldnames=fieldnames)
         thewriter.writeheader()
+
         with open("./Data/course_catalog.html", encoding="utf-8") as input_file:
             data = input_file.read()
             soup = BeautifulSoup(data, 'html.parser')
             id = 1
+
             for alink in soup.find_all('a'):
                 product_badge = alink.find('span', {'class': 'product-badge'})
                 # course_info = alink.find('div', {'class': 'card-info'})
+
                 if not product_badge == None:
                     course_description = alink.find(
                         'span', class_='card-description')
                     thewriter.writerow({
-                        'ID': id, 
-                        'FullURI': 'https://coursera.org' + alink['href'], 
-                        'ProductBadge': product_badge.text , 
+                        'ID': id,
+                        'FullURI': 'https://coursera.org' + alink['href'],
+                        'ProductBadge': product_badge.text,
                         'CourseDescription': course_description.text
-                        })
+                    })
                     id += 1
 
+# getCourseListing()
 
-getCourseListing()
+#############################################################################
+
+
+#############################################################################
+def getCourseContent():
+    myBreadCrumb = []
+
+    with open("./Data/course_listing.csv", 'r') as myCSVFile:
+        theCSVreader = csv.reader(myCSVFile)
+        next(theCSVreader)
+
+    source = requests.get(
+        'https://www.coursera.org/learn/pathophysiology').text
+    soup = BeautifulSoup(source, 'lxml')
+    for data in soup.find_all('div', class_='rc-BannerBreadcrumbs'):
+        for a in data.find_all('a'):
+            myBreadCrumb.append(a.text)
+    print(myBreadCrumb)
+
+
+getCourseContent()
+
+# <a data-click-key = "discovery.phoenix_cdp.click.breadcrumb_browse_subdomain" data-click-value = "{&quot;namespace&quot;:{&quot;app&quot;:&quot;discovery&quot;,&quot;page&quot;:&quot;phoenix_cdp&quot;,&quot;component&quot;:&quot;breadcrumb_browse_subdomain&quot;,&quot;action&quot;:&quot;click&quot;},&quot;schema_type&quot;:&quot;FRONTEND&quot;,&quot;href&quot;:&quot;/browse/life-sciences/clinical-science&quot;}" data-track = "true" data-track-app = "discovery" data-track-page = "phoenix_cdp" data-track-action = "click" data-track-component = "breadcrumb_browse_subdomain" data-track-href = "/browse/life-sciences/clinical-science" href = "/browse/life-sciences/clinical-science" to = "/browse/life-sciences/clinical-science" class = "link nostyle" target = "_blank" data-reactid = "245" > Clinical Science < /a >
