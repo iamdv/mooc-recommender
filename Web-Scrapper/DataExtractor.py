42973ab7e54cbea620e385f61ab117250d2bfcb5
Deepak Vadithala <dv@deepaks-imac.home>
2018-07-30 22:31:49 +0100

    Completed the getCourseListing function which exports the course listings to the csv file

--- Web-Scrapper/DataExtractor.py
+++ Web-Scrapper/DataExtractor.py
@@ -6,54 +6,36 @@ import requests
 import lxml
 import html5lib
 import json
+import csv
 
-# def get_course_listing(input_uri):
-#   uri_listing = []
-#   source = requests.get(input_uri).text
-#   soup = BeautifulSoup(source, 'lxml')̃
-#   for alink in soup.find_all('a'):
-#     # uri_listing.append(alink.encode('utf-8').strip())
-#     print(alink.find('div', class_ = 'card-info').encode('utf-8').strip())
-#     print('----------------------')
-#     print (alink['href'])
-#     print('----------------------')
-#     print(alink.find('span', class_ = 'product-badge'))
-#     print('----------------------')
-#     print(alink.find('span', class_ = 'card-description').encode('utf-8'))
-#     print('----------------------')̃
-#   # with open('../Data/course_listing.json', 'w') as outputfile:
-#   #   json.dump(uri_listing, outputfile)
+''' 
+getCourseListing function extracts all the courses listed
+and we save the courses in the course_listing.csv file 
+in the data folder.
+'''
 
-# get_course_listing('https://www.coursera.org/courses')
-
-
-# # source = requests.get('https://www.coursera.org/learn/python-data').text
-# source = requests.get('https://www.coursera.org/learn/machine-learning').text
-
-# soup = BeautifulSoup(source, 'lxml')
-
-# body = soup.find('body')
-
-# for aText in body.find('div', class_ = 'bt3-col-sm-9').find_all('a'):
-#   print(aText.get('href'))
-
-# for a in soup.find_all('a', href=True):
-#     print ("Found the URL:", a['href'])
-# bt3-col-sm-9
-# #266
+def getCourseListing():
+    with open("./Data/course_listing.csv", 'w', newline= '') as myCSVFile:
+        fieldnames = ['ID', 'FullURI', 'ProductBadge', 'CourseDescription']
+        thewriter = csv.DictWriter(myCSVFile, fieldnames= fieldnames)
+        thewriter.writeheader()
+        with open("./Data/course_catalog.html", encoding="utf-8") as input_file:
+            data = input_file.read()
+            soup = BeautifulSoup(data, 'html.parser')
+            id = 1
+            for alink in soup.find_all('a'):
+                product_badge = alink.find('span', {'class': 'product-badge'})
+                # course_info = alink.find('div', {'class': 'card-info'})
+                if not product_badge == None:
+                    course_description = alink.find(
+                        'span', class_='card-description')
+                    thewriter.writerow({
+                        'ID': id, 
+                        'FullURI': 'https://coursera.org' + alink['href'], 
+                        'ProductBadge': product_badge.text , 
+                        'CourseDescription': course_description.text
+                        })
+                    id += 1
 
 
-def get_course_listing():
-    with open("./Data/course_catalog.html", encoding="utf-8") as input_file:
-        data = input_file.read()
-        soup = BeautifulSoup(data, 'html.parser')
-        for alink in soup.find_all('a'):
-            product_badge = alink.find('span', {'class': 'product-badge'})
-            # course_info = alink.find('div', {'class': 'card-info'})
-            if not product_badge == None:
-                course_description = alink.find(
-                    'span', class_='card-description')
-                print(alink['href'], '|' ,product_badge.text, '|' ,course_description.text, '|')
-
-
-get_course_listing()
+getCourseListing()
