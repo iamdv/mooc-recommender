93fd47a48805bea2114639376834b1c07e8b8a96
DV <me@iamdv.io>
2018-09-01 13:16:18 +0100

    Checkpoint on Jupyter notebook. Results are better but not 100% yet

--- Model/Cosine_Distance.py
+++ Model/Cosine_Distance.py
@@ -6,6 +6,7 @@ import nltk
 import string
 import csv
 
+# nltk.download('stopwords')
 
 def normalize(text):
     """
@@ -14,7 +15,6 @@ def normalize(text):
     :param text: search text
     :return: returns the normalized text.
     """
-    nltk.download('stopwords')
     remove_punctuation_map = dict((ord(char), None)
                                   for char in string.punctuation)
     return text.lower().translate(remove_punctuation_map)
@@ -36,11 +36,34 @@ def cosine_sim(text1, text2):
 def execute_cosine(fpath_skills, fpath_courses):
     df_skills = pd.read_csv(fpath_skills, sep=',')
     df_courses = pd.read_csv(fpath_courses, sep=',')
-    for s_index, s_row in df_skills.iterrows():
-        if s_row['Role'] == 'Data Scientist':
-            for c_index, c_row in df_courses.iterrows():
-                
-            # print(row['Skills'], row['SkillWeight'])
+    test_list = []
+    for _, c_row in df_courses.iterrows():
+        c_wgtd_score = 0
+        c_wgtd_score2 = 0
+        for _, s_row in df_skills.iterrows():
+            if s_row['Role'] == 'Data Scientist':
+                # print(c_row['Course Description'].encode('utf-8'))
+                c_wgtd_score = c_wgtd_score + (
+                    cosine_sim(
+                               str(s_row['Skills']).encode('utf-8'),
+                               str(c_row['Course Name']).encode('utf-8'))
+                               * s_row['SkillWeight'])
+                c_wgtd_score2 = c_wgtd_score2 + (
+                    cosine_sim(
+                               str(s_row['Skills']).encode('utf-8'),
+                               str(c_row['Course Description']).encode('utf-8'))
+                               * s_row['SkillWeight'])                               
+        # print(c_wgtd_score)
+        test_list.append((c_row['Course Id'], c_wgtd_score, c_wgtd_score2))
+    with open("././Data/temp_output.csv",'w') as result:
+        csv_out = csv.writer(result)
+        csv_out.writerow(['Course Id','CategoryCS', 'DescriptionCS'])
+        for row in test_list:
+            csv_out.writerow(row)
+    return None
 
 
-execute_cosine('././Data/linkedin_skills_weighted.csv', '././Data/main_coursera.csv')
+print(execute_cosine('././Data/temp.csv', '././Data/main_coursera.csv'))
+# print(cosine_sim('Data Science', 'Data'))
+# print(cosine_sim('hello world .', 'This sentence is similar to a foo bar sentence'))
+# linkedin_skills_weighted
