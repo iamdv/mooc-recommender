cb5b2a375d6fff11911952155dee00478777f4d2
DV <me@iamdv.io>
2018-09-08 00:57:58 +0100

    Updated the logic for the Google Word2Vec algorithm

--- Model/Word2VecCustom.py
+++ Model/Word2VecCustom.py
@@ -1,86 +1,17 @@
 import pandas as pd
-from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
-import nltk.data
-nltk.download() 
-import logging
-from gensim.models import word2vec
+import gensim
+import os
 
 
+base_path = './Data/'
 
-# Read data from course file through pandas
+# import the pre-trained model to remove the words not in the vocabulary. We're using the open source google model trained on millions of news articles.
+model = gensim.models.KeyedVectors.load_word2vec_format(base_path+"/GoogleNews-vectors-negative300.bin",binary=True)
 
+# load the course data
 
-def clean_description(course_desc, remove_stopwords = True):
-    
-    
-    course_desc = re.sub("[^a-zA-Z]"," ", course_desc)
-    #
-    # 3. Convert words to lower case and split them
-    words = course_desc.lower().split()
-    #
-    # 4. Optionally remove stop words (True by default)
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in stops]
-    #
-    # 5. Return a list of words
-    return(words)
+sim = model.n_similarity(['data','scientist'],['data','science'])
 
-  
-'''
-# Load the punkt tokenizer
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
-# Define a function to split a course descriptions into parsed sentences
-def course_desc_to_sentences( course_desc, tokenizer, remove_stopwords=True ):
-    # Function to split a review into parsed sentences. Returns a 
-    # list of sentences, where each sentence is a list of words
-    #
-    # 1. Use the NLTK tokenizer to split the paragraph into sentences
-    raw_sentences = tokenizer.tokenize(course_desc.strip())
-    #
-    # 2. Loop over each sentence
-    sentences = []
-    for raw_sentence in raw_sentences:
-        # If a sentence is empty, skip it
-        if len(raw_sentence) > 0:
-            # Otherwise, call clean_description to get a list of words
-            sentences.append( clean_description( raw_sentence,remove_stopwords ))
-    #
-    # Return the list of sentences (each sentence is a list of words,
-    # so this returns a list of lists
-    return sentences
-
-
-sentences = []  # Initialize an empty list of sentences
-
-print "Parsing sentences from training set"
-for course_desc in course_descriptions:
-    sentences += course_desc_to_sentences(course_desc, tokenizer)
-
-
-# Training and saving the model.
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
-
-# Set values for various parameters
-num_features = 300    # Word vector dimensionality                      
-min_word_count = 40   # Minimum word count                        
-num_workers = 4       # Number of threads to run in parallel
-context = 10          # Context window size                                                                                    
-downsampling = 1e-3   # Downsample setting for frequent words
-
-# Initialize and train the model (this will take some time)
-print "Training model..."
-model = word2vec.Word2Vec(sentences, workers=num_workers,size=num_features, min_count = min_word_count, window = context, sample = downsampling)
-
-model.init_sims(replace=True)
-model_name = "300features_40minwords_10context"
-model.save(model_name)
-
-
-# Now the saved model can be used just like the pre-trained google model. 
-
-'''
+print(sim)
--- Model/Word2VecGoogle.py
+++ Model/Word2VecGoogle.py
@@ -1,20 +1,77 @@
 import pandas as pd
-import re
+import nltk
 from nltk.corpus import stopwords
+from nltk import word_tokenize
+import string
 import gensim
 import os
+import csv
+import re
 
 
+pd.set_option('display.max_columns', 10)
 base_path = './Data/'
+
 
 '''
 Import the pre-trained model to remove the words not in the vocabulary.
 We will use the open source google model trained on millions of news articles.
 '''
-
 model = gensim.models.KeyedVectors.load_word2vec_format(
-    base_path + "GoogleNews-vectors-negative300.bin", binary = True)
+    base_path + "GoogleNews-vectors-negative300.bin", binary=True)
 
-sim = model.n_similarity(['data', 'science'],['data', 'scientist'])
 
-print(base_path)
+def word2vec_similarity(my_skills, my_coursedesc):
+    return model.n_similarity(my_skills, my_coursedesc)
+
+
+def execute_word2vecGoogle(fpath_skills, fpath_courses, my_role, output_fname):
+    stop = stopwords.words('english') + list(string.punctuation)
+    df_skills = pd.read_csv(fpath_skills, sep=',')
+    df_skills = df_skills[(df_skills['Role'] == my_role)]
+    df_courses = pd.read_csv(fpath_courses, sep=',')
+    course_text_final = []
+
+    word2vec_score = []
+
+    for _, c_row in df_courses.iterrows():
+        c_wgtd_role_score = 0
+        c_wgtd_skill_score = 0
+        course_text = (str(c_row['Category']) + ' '
+        + str(c_row['Course Name']) + ' '
+        + str(c_row['Course Description']))
+
+        course_text = [i for i in word_tokenize(course_text.lower()) if i not in stop]
+        course_text = [word for word in course_text if word in model.vocab]
+
+        for _, s_row in df_skills.iterrows():
+            skill = str(s_row['Skills']).strip()
+            skill_weight = s_row['SkillWeight']
+            role_list = str(s_row['Role']).split()
+            course_name_list = str(c_row['Course Name'])
+
+            course_name_list = [i for i in word_tokenize(course_name_list.lower()) if i not in stop]
+            course_name_list = [word for word in course_name_list if word in model.vocab]
+                      
+            skill = [i for i in word_tokenize(skill.lower()) if i not in stop]
+            skill = [word for word in skill if word in model.vocab]
+
+            try:
+                c_wgtd_skill_score = c_wgtd_skill_score + (word2vec_similarity(skill, course_text) * skill_weight)
+                print('Skill is ', skill, ' and score :', c_wgtd_skill_score)
+                c_wgtd_role_score = word2vec_similarity(role_list, course_name_list)
+            except ZeroDivisionError:
+                pass
+            
+        word2vec_score.append((c_row['Course Id'], my_role, c_wgtd_skill_score, c_wgtd_role_score))
+
+    with open("././Data/Word2Vec-Google/" + output_fname,'w') as result:
+        csv_out = csv.writer(result)
+        csv_out.writerow(['Course Id', 'Role', 'Skill_Score','Role_Score'])
+        for row in word2vec_score:
+            csv_out.writerow(row) 
+    return None
+
+
+print(execute_word2vecGoogle('././Data/Temp_linkedin_skills_weighted.csv',
+'././Data/Temp_Courses.csv', 'Data Scientist', 'Word2VecGoogle_DataScientist.csv'))
